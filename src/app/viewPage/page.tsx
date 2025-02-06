508243df97a3d1eaedfd2140a56857c28275817d
24LP8639 24LP8639 <24LP8639@G305-employeeiMac.local>
2025-02-06 17:39:15 +0800

    viewpage

--- src/app/viewPage/page.tsx
+++ src/app/viewPage/page.tsx
@@ -139,44 +139,44 @@ export default function ViewPage() {
       <div className="w-full flex justify-center gap-6 -my-20 relative">
         <div className="w-[60rem] h-[50rem] bg-[#ffffff] rounded-md">
           <div className="min-h-[20rem] gap-3 border rounded-md p-4">
-            <div className="flex justify-between border-b-[1px] pb-6">
+            <div className="flex justify-between border-b-[1px] pb-6 mt-4">
               <div className="flex items-center gap-2">
                 <img src="Avatar-Image.png" alt="" />
-                <p className="font-bold">Jake</p>
+                <p className="font-bold text-2xl">Jake</p>
               </div>
               <button className="w-[6.25rem] rounded-md bg-[#F4F4F5] p-2">
                 Edit page
               </button>
             </div>
-            <div className="mt-10">
+            <div className="mt-10 ml-4">
               <p className="text-xl font-semibold">About Jake</p>
-              <p className="max-w-[39.5rem] text-lg mt-4">
+              <p className="max-w-[39.5rem] mt-4">
                 I'm a typical person who enjoys exploring different things. I
                 also make music art as a hobby. Follow me along.
               </p>
             </div>
           </div>
           <div className="h-[10rem] gap-3 border rounded-md mt-4 p-4">
-            <p className="font-md">Social media URL</p>
+            <p className="font-md mt-4">Social media URL</p>
             <input
-              className="w-full mt-2 p-2 border rounded-md"
+              className="w-full mt-4 p-2 border rounded-md"
               type="url"
               placeholder="https://buymecoffee.com/spacerulz44"
             />
           </div>
           <div className="h-[20rem] gap-6 border rounded-md mt-4 p-4">
-            <p className="font-md">Recent Supporters</p>
-            <div className="w-full min-h-[10rem] border rounded-md mt-4 flex flex-col items-center justify-center">
-              <FaHeart className="" />
+            <p className="font-md mt-4">Recent Supporters</p>
+            <div className="w-full min-h-[10rem] border rounded-md mt-6 flex flex-col items-center justify-center">
+              <FaHeart />
               <p className="mt-2">Be the first one to support Jake</p>
             </div>
           </div>
         </div>
-        <div className="w-[55rem] min-h-[40rem] p-6 bg-[#ffffff] border rounded-md">
-          <div className="mb-6">
-            <p className="text-xl font-bold">Buy Jake a Coffee</p>
-            <p className="text-[#09090B] font-md mt-4">Select amount:</p>
-            <div className="flex gap-4 mt-3">
+        <div className="w-[55rem] h-[40rem] p-6 bg-[#ffffff] border rounded-md">
+          <div className="mb-4">
+            <p className="text-xl font-bold mt-4">Buy Jake a Coffee</p>
+            <p className="text-[#09090B] font-md mt-5">Select amount:</p>
+            <div className="flex gap-4 mt-4">
               {[1, 2, 5, 10].map((amount) => (
                 <button
                   key={amount}
@@ -187,24 +187,24 @@ export default function ViewPage() {
               ))}
             </div>
           </div>
-          <div className="mb-6">
-            <p className="text-[#09090B]">
+          <div className="mb-7">
+            <p className="text-[#09090B] mt-4">
               Enter BuyMeCoffee or social account URL:
             </p>
             <input
-              className="w-full h-12 border rounded-md px-4 mt-2"
-              placeholder="@gmail.com"
+              className="w-full h-12 border rounded-md px-4 mt-4"
+              placeholder="bymecoffee@gmail.com"
               type="url"
             />
           </div>
-          <div className="mb-4">
+          <div className="mb-6">
             <p className="text-[#09090B]">Special message:</p>
             <textarea
-              className="w-full h-32 border rounded-md px-4 py-2 mt-2"
+              className="w-full h-32 border rounded-md px-4 py-2 mt-4"
               placeholder="Please write your message here"
             />
           </div>
-          <button className="w-full h-12 bg-[#18181B] opacity-20 text-white rounded-md font-md hover:bg-[#18181B]">
+          <button className="w-full h-12 bg-[#18181B] opacity-20 text-white mt-4 rounded-md font-md hover:bg-[#18181B]">
             Support
           </button>
         </div>
