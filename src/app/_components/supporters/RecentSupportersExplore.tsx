1e43ac446cc166a32ced10b3ceef9a16d09cdb62
Munkhtsolmon Munkhsaikhan <155965525+Munkhtsolmon2222@users.noreply.github.com>
2025-02-10 13:54:20 +0800

    Viewpage (#15)
    * viewpage
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: 24LP8639 24LP8639 <24LP8639@G305-employeeiMac.local>
    Co-authored-by: Solongooyaa <u.solongo2018@gmail.com>
    Co-authored-by: 24LP8639 24LP8639 <24LP8639@G305-employeeiMac.localdomain>

--- src/app/_components/supporters/RecentSupportersExplore.tsx
+++ src/app/_components/supporters/RecentSupportersExplore.tsx
@@ -1,32 +1,48 @@
+"use client";
+import ViewPage from "@/app/viewPage/page";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 import { FiExternalLink } from "react-icons/fi";
 
 export default function RecentSupportExplore() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const router = useRouter();
+
   return (
-    <div className="max-w-[909px] mx-auto mt-16 p-5 max-h-56 border border-solid rounded-lg ">
-      <div className="flex justify-between ">
-        <div className="flex gap-3 items-center">
-          <img src="Avatar-Image.png" />
-          <h4 className="font-semibold">Baaska</h4>
+    <div>
+      {modalOpen ? (
+        <ViewPage onClose={() => setModalOpen(false)} isEdit={false} />
+      ) : (
+        <div className="max-w-[909px] mx-auto mt-16 p-5 max-h-56 border border-solid rounded-lg">
+          <div className="flex justify-between">
+            <div className="flex gap-3 items-center">
+              <img src="Avatar-Image.png" />
+              <h4 className="font-semibold">Baaska</h4>
+            </div>
+            <button
+              onClick={() => setModalOpen(true)}
+              className="flex max-h-10 items-center gap-3 p-4 bg-[#f4f4f5] rounded-md"
+            >
+              View profile <FiExternalLink />
+            </button>
+          </div>
+          <div className="flex pt-5 gap-5">
+            <div>
+              <h4 className="font-bold pr-6">About Baaska</h4>
+              <p className="max-w-[420px] pt-4">
+                End minii tuhai ym bichih bogood bag hamt olondoo bayrlalaa gej
+                helyee manai bagiinhan uneheer sain baigaa shuu
+              </p>
+            </div>
+            <div>
+              <h4 className="font-semibold">Social Media URL</h4>
+              <p className="pt-4">
+                https://monkhtsolmons-organization.gitbook.io/monkhtsolmons-organization/folder-structure
+              </p>
+            </div>
+          </div>
         </div>
-        <button className="flex max-h-10 items-center gap-3 p-4 bg-[#f4f4f5] rounded-md">
-          View profile <FiExternalLink />
-        </button>
-      </div>
-      <div className="flex pt-5 gap-5">
-        <div>
-          <h4 className="font-bold pr-6 ">About Baaska</h4>
-          <p className="max-w-[420px] pt-4">
-            End minii tuhai ym bichih bogood bag hamt olondoo bayrlalaa gej
-            helyee manai bagiinhan uneheer sain baigaa shuu
-          </p>
-        </div>
-        <div>
-          <h4 className="font-semibold">Social Media URL</h4>
-          <p className="pt-4">
-            https://monkhtsolmons-organization.gitbook.io/monkhtsolmons-organization/folder-structure
-          </p>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
