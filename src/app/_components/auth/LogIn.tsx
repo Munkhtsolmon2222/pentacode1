3d62f01540a3a925ba81c12b36af777999fc2361
24LP0846 24LP0846 <24LP0846@G302-CC0089.local>
2025-02-14 16:11:50 +0800

    up

--- src/app/_components/auth/LogIn.tsx
+++ src/app/_components/auth/LogIn.tsx
@@ -6,12 +6,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
@@ -22,157 +22,160 @@ import { useAuth } from "../context/AuthContext";
 import { getCookie } from "cookies-next";
 import { jwtDecode } from "jwt-decode";
 const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-plus-jakarta",
+	subsets: ["latin"],
+	weight: ["400", "500", "700"],
+	variable: "--font-plus-jakarta",
 });
 
 const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+	email: z.string().email(),
+	password: z.string().min(8),
 });
 
 export function Login() {
-  const cookies = useCookies();
-  const [type, setType] = useState("password");
-  const [error, setError] = useState("");
-  const { setUserId } = useAuth();
+	const [type, setType] = useState("password");
+	const [error, setError] = useState("");
+	const { setUserId } = useAuth();
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
 
-  const verifyUser = async (email: string, password: string) => {
-    try {
-      const response = await fetch("http://localhost:5000/auth/sign-in", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-        credentials: "include",
-      });
+	const verifyUser = async (email: string, password: string) => {
+		try {
+			const response = await fetch("http://localhost:5000/auth/sign-in", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email, password }),
+				credentials: "include",
+			});
 
-      const data = await response.json();
-      console.log("Response:", data);
+			const data = await response.json();
+			console.log("Response:", data);
 
-      if (data.message === "Email not found") {
-        console.log("Error: Email does not exist.");
-        setError("Email does not exist.");
-      } else if (data.message === "Incorrect password") {
-        console.log("Error: Password is incorrect.");
-        setError("Password is incorrect.");
-      } else {
-        console.log("Login successful!");
-        setError("Login successful!");
-        // setUserId(data.);
-        localStorage.setItem("userId", data?.data?.accessToken);
-        // console.log(data.id);
-        router.push("/home");
-      }
-    } catch (error) {
-      console.error("Error verifying user:", error);
-    }
-  };
+			if (data.message === "Email not found") {
+				console.log("Error: Email does not exist.");
+				setError("Email does not exist.");
+			} else if (data.message === "Incorrect password") {
+				console.log("Error: Password is incorrect.");
+				setError("Password is incorrect.");
+			} else {
+				console.log("Login successful!");
+				setError("Login successful!");
+				// setUserId(data.);
+				localStorage.setItem("userId", data?.data?.accessToken);
+				// console.log(data.id);
+				router.push("/home");
+			}
+		} catch (error) {
+			console.error("Error verifying user:", error);
+		}
+	};
 
-  const accessToken = getCookie("accessToken") || "";
-  console.log({ accessToken });
-  const decoded = jwtDecode(accessToken);
-  console.log(decoded.userId);
+	const accessToken = getCookie("accessToken") || "";
+	let refreshToken;
+	console.log({ accessToken });
+	const decoded = jwtDecode(accessToken);
+	if (!accessToken) {
+		refreshToken = getCookie("refreshToken") || "";
+	}
+	console.log(decoded.userId);
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    verifyUser(values.email, values.password);
-  }
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		verifyUser(values.email, values.password);
+	}
 
-  function changeType() {
-    setType((prevType) => (prevType === "password" ? "text" : "password"));
-  }
+	function changeType() {
+		setType((prevType) => (prevType === "password" ? "text" : "password"));
+	}
 
-  return (
-    <div className={`${plusJakartaSans.variable} font-sans min-h-screen`}>
-      <div className="flex justify-end items-center p-6 mx-6">
-        <Link href="/signup">
-          <Button variant="secondary">Sign up</Button>
-        </Link>
-      </div>
-      <div className="max-w-md mx-auto mt-[180px] text-black space-y-4">
-        <div className="w-[360px] mx-auto text-black">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <p className="text-[24px] font-bold">Welcome back</p>
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      <div className="font-bold">Email</div>
-                    </FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter email here" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    {error == "Email does not exist." && (
-                      <div className="block mx-auto w-[90%] text-red-500 text-[12px]">
-                        {error}
-                      </div>
-                    )}
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      <div className="font-bold">Password</div>
-                    </FormLabel>
-                    <FormControl>
-                      <div className="relative">
-                        <Input
-                          placeholder="Enter password here"
-                          type={type}
-                          {...field}
-                        />
-                        <div
-                          onClick={changeType}
-                          className="absolute top-2 left-[300px] text-gray-400 hover:text-gray-600 cursor-pointer"
-                        >
-                          {type === "text" ? <Eye /> : <EyeClosed />}
-                        </div>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                    {error == "Password is incorrect." && (
-                      <div className="block mx-auto w-[90%] text-red-500 text-[12px]">
-                        {error}
-                        <Link href="/forgot-password">
-                          <p className="text-black hover:underline">
-                            Forgot password?
-                          </p>
-                        </Link>
-                      </div>
-                    )}
-                  </FormItem>
-                )}
-              />
-              <Button
-                className="block mx-auto w-full p-2 bg-primary text-white"
-                type="submit"
-              >
-                Continue
-              </Button>
-            </form>
-          </Form>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`${plusJakartaSans.variable} font-sans min-h-screen`}>
+			<div className="flex justify-end items-center p-6 mx-6">
+				<Link href="/signup">
+					<Button variant="secondary">Sign up</Button>
+				</Link>
+			</div>
+			<div className="max-w-md mx-auto mt-[180px] text-black space-y-4">
+				<div className="w-[360px] mx-auto text-black">
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+							<p className="text-[24px] font-bold">Welcome back</p>
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>
+											<div className="font-bold">Email</div>
+										</FormLabel>
+										<FormControl>
+											<Input placeholder="Enter email here" {...field} />
+										</FormControl>
+										<FormMessage />
+										{error == "Email does not exist." && (
+											<div className="block mx-auto w-[90%] text-red-500 text-[12px]">
+												{error}
+											</div>
+										)}
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>
+											<div className="font-bold">Password</div>
+										</FormLabel>
+										<FormControl>
+											<div className="relative">
+												<Input
+													placeholder="Enter password here"
+													type={type}
+													{...field}
+												/>
+												<div
+													onClick={changeType}
+													className="absolute top-2 left-[300px] text-gray-400 hover:text-gray-600 cursor-pointer"
+												>
+													{type === "text" ? <Eye /> : <EyeClosed />}
+												</div>
+											</div>
+										</FormControl>
+										<FormMessage />
+										{error == "Password is incorrect." && (
+											<div className="block mx-auto w-[90%] text-red-500 text-[12px]">
+												{error}
+												<Link href="/forgot-password">
+													<p className="text-black hover:underline">
+														Forgot password?
+													</p>
+												</Link>
+											</div>
+										)}
+									</FormItem>
+								)}
+							/>
+							<Button
+								className="block mx-auto w-full p-2 bg-primary text-white"
+								type="submit"
+							>
+								Continue
+							</Button>
+						</form>
+					</Form>
+				</div>
+			</div>
+		</div>
+	);
 }
