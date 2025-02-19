c4d99dc8db9b658815da610597e417abd6ddc670
Khosbayar707 <torutora0415@gmail.com>
2025-02-19 13:34:18 +0800

    add loading in sign up

--- src/app/_components/auth/LogIn.tsx
+++ src/app/_components/auth/LogIn.tsx
@@ -133,13 +133,13 @@ export function Login() {
                           placeholder="Enter password here"
                           {...field}
                         />
-                        <Button
+                        <button
                           type="button"
                           onClick={() => setShowPassword(!showPassword)}
                           className="absolute right-3 top-2 text-gray-500 hover:text-gray-700"
                         >
                           {showPassword ? <Eye /> : <EyeClosed />}
-                        </Button>
+                        </button>
                       </div>
                     </FormControl>
                     <FormMessage />
