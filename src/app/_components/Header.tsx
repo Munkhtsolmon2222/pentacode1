fe8a0677747be3835a4b607a507bececd2586408
24LP7757 24LP7757 <24LP7757@G302-CC0093.local>
2025-02-19 18:13:41 +0800

    some +

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -6,108 +6,108 @@ import { User } from "../constants/type";
 import { getCookie, deleteCookie } from "cookies-next";
 import { ChevronDown } from "lucide-react";
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { accessToken } from "@/utils/accessToken";
 import { usePathname } from "next/navigation";
 
 export default function Header() {
-	const [userData, setUserData] = useState<User | null>(null);
-	const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
-	const [userId, setUserId] = useState<string | null>(null);
-	const pathName = usePathname();
-	useEffect(() => {
-		const storedUserId: string | null = localStorage.getItem("userId");
-		setUserId(storedUserId);
-	}, []);
+  const [userData, setUserData] = useState<User | null>(null);
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const [userId, setUserId] = useState<string | null>(null);
+  const pathName = usePathname();
+  useEffect(() => {
+    const storedUserId: string | null = localStorage.getItem("userId");
+    setUserId(storedUserId);
+  }, []);
 
-	const fetchData = async () => {
-		console.log("fetchData");
-		try {
-			if (!userId) return;
-			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_API_URL}/profile/currentuser/${userId}`,
-				{
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: "Bearer " + accessToken,
-					},
-				}
-			);
-			if (!res.ok) throw new Error("Failed to fetch user data");
-			const resJson = await res.json();
-			console.log(resJson);
-			setUserData(resJson);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const fetchData = async () => {
+    console.log("fetchData");
+    try {
+      if (!userId) return;
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/profile/currentuser/${userId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+      if (!res.ok) throw new Error("Failed to fetch user data");
+      const resJson = await res.json();
+      console.log(resJson);
+      setUserData(resJson);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	useEffect(() => {
-		console.log("useeffect");
-		fetchData();
-	}, [pathName]);
+  useEffect(() => {
+    console.log("useeffect");
+    fetchData();
+  }, [pathName]);
 
-	const logout = () => {
-		deleteCookie("accessToken");
-		deleteCookie("refreshToken");
-		setUserData(null);
-		setDropdownOpen(false);
-		window.location.href = "/login";
-	};
-	console.log(userData);
-	return (
-		<div className="bg-white p-4 absolute top-0 w-full z-10">
-			<div className="flex justify-between w-[90%] mx-20">
-				<div className="flex gap-2 items-center font-bold">
-					<Link href="/">
-						<div className="flex gap-2 items-center font-bold h-10">
-							<LuCoffee className="text-2xl" />
-							Buy Me Coffee
-						</div>
-					</Link>
-				</div>
+  const logout = () => {
+    deleteCookie("accessToken");
+    deleteCookie("refreshToken");
+    setUserData(null);
+    setDropdownOpen(false);
+    window.location.href = "/login";
+  };
+  console.log(userData);
+  return (
+    <div className="bg-white p-4 absolute top-0 w-full z-10">
+      <div className="flex justify-between w-[90%] mx-20">
+        <div className="flex gap-2 items-center font-bold">
+          <Link href="/">
+            <div className="flex gap-2 items-center font-bold h-10">
+              <LuCoffee className="text-2xl" />
+              Buy Me Coffee
+            </div>
+          </Link>
+        </div>
 
-				<div className="flex justify-between items-center max-w-[168px] relative">
-					{userData && (
-						<div
-							className="flex items-center gap-2 cursor-pointer"
-							onClick={() => setDropdownOpen((prev) => !prev)}
-						>
-							<img
-								className="w-10 h-10 rounded-full"
-								src={userData.avatarImage}
-								alt="User Avatar"
-							/>
-							<h4>{userData.name}</h4>
-							<div className="mt-2">
-								<DropdownMenu>
-									<DropdownMenuTrigger>
-										<ChevronDown />
-									</DropdownMenuTrigger>
-									<DropdownMenuContent>
-										<DropdownMenuLabel>My Account</DropdownMenuLabel>
-										<DropdownMenuSeparator />
-										<DropdownMenuItem>
-											<button
-												onClick={logout}
-												className="w-full text-red-600 font-bold py-2 px-4 text-left rounded-md"
-											>
-												Log out
-											</button>
-										</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+        <div className="flex justify-between items-center max-w-[168px] h-10 relative">
+          {userData && (
+            <div
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={() => setDropdownOpen((prev) => !prev)}
+            >
+              <img
+                className="w-10 h-10 rounded-full "
+                src={userData.avatarImage}
+                alt="User Avatar"
+              />
+              <h4 className=" whitespace-nowrap ">{userData.name}</h4>
+              <div className="mt-2">
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <ChevronDown />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <button
+                        onClick={logout}
+                        className="w-full text-red-600 font-bold py-2 px-4 text-left rounded-md"
+                      >
+                        Log out
+                      </button>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/_components/UserProfile.tsx
+++ src/app/_components/UserProfile.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { LuCopy } from "react-icons/lu";
 import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { User } from "../constants/type";
 import { useEffect, useState } from "react";
@@ -14,127 +14,130 @@ import RecentSupport from "./supporters/RecentSupportersHome";
 import { accessToken } from "@/utils/accessToken";
 
 export default function UserProfile() {
-	const [userData, setUserData] = useState<User | null>(null);
-	const [totalValue, setTotalValue] = useState<string | null>(null);
-	const [userValue, setUserValue] = useState({
-		totalEarnings30Days: 0,
-		totalEarnings90Days: 0,
-		totalEarningsAllTime: 0,
-	});
-	const [userId, setUserId] = useState<string | null>(null);
-	useEffect(() => {
-		const storedUserId: string | null = localStorage.getItem("userId");
-		setUserId(storedUserId);
-	}, []);
-	useEffect(() => {
-		fetchData();
-		fetchTotalEarnings();
-	}, [userId]);
-	const fetchData = async () => {
-		if (!userId) {
-			console.warn("No userId found, skipping fetch.");
-			return;
-		}
-		try {
-			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_API_URL}/profile/currentuser/${userId}`,
-				{
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: "Bearer " + accessToken,
-					},
-				}
-			);
-			if (!res.ok) throw new Error("Failed to fetch user data");
-			const resJson = await res.json();
-			setUserData(resJson);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const [userData, setUserData] = useState<User | null>(null);
+  const [totalValue, setTotalValue] = useState<string | null>(null);
+  const [userValue, setUserValue] = useState({
+    totalEarnings30Days: 0,
+    totalEarnings90Days: 0,
+    totalEarningsAllTime: 0,
+  });
+  const [userId, setUserId] = useState<string | null>(null);
+  useEffect(() => {
+    const storedUserId: string | null = localStorage.getItem("userId");
+    setUserId(storedUserId);
+  }, []);
+  useEffect(() => {
+    fetchData();
+    fetchTotalEarnings();
+  }, [userId]);
+  const fetchData = async () => {
+    if (!userId) {
+      console.warn("No userId found, skipping fetch.");
+      return;
+    }
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/profile/currentuser/${userId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+      if (!res.ok) throw new Error("Failed to fetch user data");
+      const resJson = await res.json();
+      setUserData(resJson);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	const fetchTotalEarnings = async () => {
-		try {
-			const response = await fetch(
-				`${process.env.NEXT_PUBLIC_API_URL}/donation/total-earnings/${userId}`,
-				{
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: "Bearer " + accessToken,
-					},
-				}
-			);
-			if (!response.ok) throw new Error("Failed to fetch earnings data");
-			const resJson = await response.json();
-			setUserValue({
-				totalEarnings30Days: resJson.totalEarnings30Days,
-				totalEarnings90Days: resJson.totalEarnings90Days,
-				totalEarningsAllTime: resJson.totalEarningsAllTime,
-			});
-		} catch (error) {
-			console.error(error);
-		}
-	};
-	const userLink = `http://localhost:3000/user/${userId}`;
-	const handleShareLink = () => {
-		navigator.clipboard
-			.writeText(userLink)
-			.then(() => {
-				alert("Link copied to clipboard!");
-			})
-			.catch((err) => {
-				console.error("Failed to copy: ", err);
-			});
-	};
-	const renderEarnings = () => {
-		if (totalValue === "30day") {
-			return `$${userValue.totalEarnings30Days || "0"}`;
-		} else if (totalValue === "90days") {
-			return `$${userValue.totalEarnings90Days || "0"}`;
-		} else if (totalValue === "Alltime") {
-			return `$${userValue.totalEarningsAllTime || "0"}`;
-		}
-		return "$0";
-	};
+  const fetchTotalEarnings = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/donation/total-earnings/${userId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+      if (!response.ok) throw new Error("Failed to fetch earnings data");
+      const resJson = await response.json();
+      setUserValue({
+        totalEarnings30Days: resJson.totalEarnings30Days,
+        totalEarnings90Days: resJson.totalEarnings90Days,
+        totalEarningsAllTime: resJson.totalEarningsAllTime,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const userLink = `http://localhost:3000/user/${userId}`;
+  const handleShareLink = () => {
+    navigator.clipboard
+      .writeText(userLink)
+      .then(() => {
+        alert("Link copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+      });
+  };
+  const renderEarnings = () => {
+    if (totalValue === "30day") {
+      return `$${userValue.totalEarnings30Days || "0"}`;
+    } else if (totalValue === "90days") {
+      return `$${userValue.totalEarnings90Days || "0"}`;
+    } else if (totalValue === "Alltime") {
+      return `$${userValue.totalEarningsAllTime || "0"}`;
+    }
+    return "$0";
+  };
 
-	return (
-		<div className="w-full bg-gray-primary text-black p-4">
-			<div className="max-w-[80%] mx-auto mt-16 p-5 border border-solid rounded-lg">
-				<div className="flex justify-between items-center">
-					<div className="w-12 h-12 rounded-full flex gap-3 items-center">
-						<img src={userData?.avatarImage} className="rounded-full" />
-						<div className="flex flex-col">
-							<h4 className="font-semibold">{userData?.name}</h4>
-							<a className="text-sm">{userData?.socialMediaURL}</a>
-						</div>
-					</div>
-					<button
-						onClick={handleShareLink}
-						className="flex max-h-10 items-center text-white gap-3 p-4 bg-[#18181B] rounded-md"
-					>
-						<LuCopy /> Share page link
-					</button>
-				</div>
-				<div className="max-w-[80%] mx-auto border border-solid bg-[#E4E4E7] mt-6"></div>
-				<div className="flex items-center gap-4 pt-7">
-					<h4 className="font-bold text-2xl">Earning</h4>
-					<Select onValueChange={setTotalValue}>
-						<SelectTrigger className="w-[180px]">
-							<SelectValue className="text-lg" placeholder="Last 30 days" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								<SelectItem value="30day">Last 30 days</SelectItem>
-								<SelectItem value="90days">Last 90 days</SelectItem>
-								<SelectItem value="Alltime">All time</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-				</div>
-				<p className="font-bold text-4xl size-9 pt-6 pb-10">
-					{renderEarnings()}
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full bg-gray-primary text-black p-4">
+      <div className="max-w-[80%] mx-auto mt-16 p-5 border border-solid rounded-lg">
+        <div className="flex justify-between items-center">
+          <div className="w-12 h-12 rounded-full flex gap-3 items-center">
+            <img
+              src={userData?.avatarImage}
+              className=" h-10 w-10 rounded-full"
+            />
+            <div className="flex flex-col">
+              <h4 className="font-semibold">{userData?.name}</h4>
+              <a className="text-sm">{userData?.socialMediaURL}</a>
+            </div>
+          </div>
+          <button
+            onClick={handleShareLink}
+            className="flex max-h-10 items-center text-white gap-3 p-4 bg-[#18181B] rounded-md"
+          >
+            <LuCopy /> Share page link
+          </button>
+        </div>
+        <div className="max-w-[80%] mx-auto border border-solid bg-[#E4E4E7] mt-6"></div>
+        <div className="flex items-center gap-4 pt-7">
+          <h4 className="font-bold text-2xl">Earning</h4>
+          <Select onValueChange={setTotalValue}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue className="text-lg" placeholder="Last 30 days" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="30day">Last 30 days</SelectItem>
+                <SelectItem value="90days">Last 90 days</SelectItem>
+                <SelectItem value="Alltime">All time</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <p className="font-bold text-4xl size-9 pt-6 pb-10">
+          {renderEarnings()}
+        </p>
+      </div>
+    </div>
+  );
 }
