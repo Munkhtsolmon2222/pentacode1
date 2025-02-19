e67a69cb3efe6838951006a22f4ecc937fafa48c
Khosbayar707 <torutora0415@gmail.com>
2025-02-19 14:51:24 +0800

    skeleton

---
+++ src/app/_components/Skeleton.tsx
@@ -0,0 +1,24 @@
+export function Skeleton() {
+  return (
+    <div className="w-[80%] mx-auto mt-16 p-5 h-40 bg-gray-100 animate-pulse rounded-lg">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          <div className="w-10 h-10 rounded-full bg-gray-300 animate-pulse"></div>
+          <div className="w-24 h-5 bg-gray-300 animate-pulse"></div>
+        </div>
+        <div className="w-24 h-8 bg-gray-300 rounded-md animate-pulse"></div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 pt-6">
+        <div>
+          <div className="w-36 h-4 bg-gray-300 animate-pulse"></div>
+          <p className="w-full h-4 mt-2 bg-gray-300 animate-pulse"></p>
+        </div>
+        <div>
+          <div className="w-36 h-4 bg-gray-300 animate-pulse"></div>
+          <div className="w-48 h-4 mt-2 bg-gray-300 animate-pulse"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
