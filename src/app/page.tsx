991e4d5d0c61d099bf8e898a7255df596363fe44
24LP7757 24LP7757 <24LP7757@G302-CC0093.local>
2025-02-19 15:53:45 +0800

    pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-[80%] fixed right-0 top-0 h-full bg-gray-primary text-black p-4">
-      "main page" goy yum hiine
-      {/* <Complete /> */}
-    </div>
+    <div className="w-[80%] fixed right-0 top-0 h-full bg-gray-primary text-black p-4"></div>
   );
 }
