87143c592b6bd66e59d6ae99525897239aae344c
Khosbayar707 <torutora0415@gmail.com>
2025-02-20 17:54:41 +0800

    hom epage update

--- src/app/_components/supporters/RecentSupportersHome.tsx
+++ src/app/_components/supporters/RecentSupportersHome.tsx
@@ -4,75 +4,95 @@ import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 
 export default function RecentSupport({
-	transaction,
+  transaction,
+  userId,
 }: {
-	transaction: Transaction;
+  transaction: Transaction;
+  userId: string | null;
 }) {
-	const [userData, setUserData] = useState<User | null>(null);
-	const accessToken = Cookies.get("accessToken");
+  const [userData, setUserData] = useState<User | null>(null);
+  const accessToken = Cookies.get("accessToken");
 
-	const profileFetchData = async () => {
-		try {
-			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_API_URL}/profile/viewHome/${transaction.donorId}`,
-				{
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: "Bearer " + accessToken,
-					},
-				}
-			);
-			if (!res.ok) throw new Error("Failed to fetch user data");
-			const resJson = await res.json();
-			setUserData(resJson);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const profileFetchData = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/profile/viewHome/${transaction.donorId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+      if (!res.ok) throw new Error("Failed to fetch user data");
+      const resJson = await res.json();
+      setUserData(resJson);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	useEffect(() => {
-		profileFetchData();
-	}, [transaction]);
+  useEffect(() => {
+    profileFetchData();
+  }, [transaction]);
 
-	const formatDate = (dateString: string) => {
-		const date = new Date(dateString);
-		return date.toLocaleString("en-US", {
-			year: "numeric",
-			month: "long",
-			day: "numeric",
-			hour: "2-digit",
-			minute: "2-digit",
-		});
-	};
+  const timeAgo = (date: string | Date): string => {
+    const now = new Date();
+    const past = new Date(date);
+    const diffInSeconds = Math.floor((now.getTime() - past.getTime()) / 1000);
 
-	return (
-		<div className="w-full mx-auto mt-4 p-5 border border-solid rounded-lg">
-			<div className="flex justify-between items-center">
-				<div className="flex gap-4 items-center">
-					<img
-						className="w-10 h-10 rounded-full"
-						src={userData?.avatarImage}
-						alt={`${userData?.name}'s avatar`}
-					/>
-					<div>
-						<h1 className="font-semibold text-sm">{userData?.name}</h1>
-						<p className="text-xs text-gray-500">
-							{transaction?.socialURLOrBuyMeACoffee}
-						</p>
-					</div>
-				</div>
-				<div className="text-end">
-					<h3 className="font-bold text-green-600">+ ${transaction?.amount}</h3>
-					<p className="text-xs text-gray-500">
-						{formatDate(transaction?.updatedAt)}
-					</p>
-				</div>
-			</div>
-			{transaction?.specialMessage && (
-				<p className="pt-4 text-sm text-gray-700">
-					{transaction?.specialMessage}
-				</p>
-			)}
-		</div>
-	);
+    if (diffInSeconds < 60) return `${diffInSeconds} seconds ago`;
+    if (diffInSeconds < 3600)
+      return `${Math.floor(diffInSeconds / 60)} minutes ago`;
+    if (diffInSeconds < 86400)
+      return `${Math.floor(diffInSeconds / 3600)} hours ago`;
+    return `${Math.floor(diffInSeconds / 86400)} days ago`;
+  };
+
+  return (
+    <div className="w-full mx-auto mt-4 p-5 border border-solid rounded-lg">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-4 items-center">
+          <img
+            className="w-10 h-10 rounded-full"
+            src={userData?.avatarImage}
+            alt={`${userData?.name}'s avatar`}
+          />
+          <div>
+            <h1 className="font-semibold text-sm">{userData?.name}</h1>
+            <p className="text-xs text-gray-500">
+              {transaction?.socialURLOrBuyMeACoffee}
+            </p>
+          </div>
+        </div>
+        <div className="text-end">
+          {transaction.donorId == userId && (
+            <div>
+              <h3 className="font-bold text-green-600">
+                + ${transaction?.amount}
+              </h3>
+              <p className="text-xs text-gray-500">
+                {timeAgo(transaction?.updatedAt)}
+              </p>
+            </div>
+          )}
+          {transaction.donorId !== userId && (
+            <div>
+              <h3 className="font-bold text-red-600">
+                - ${transaction?.amount}
+              </h3>
+              <p className="text-xs text-gray-500">
+                {timeAgo(transaction?.updatedAt)}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+      {transaction?.specialMessage && (
+        <p className="pt-4 text-sm text-gray-700">
+          {transaction?.specialMessage}
+        </p>
+      )}
+    </div>
+  );
 }
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,114 +5,120 @@ import UserProfile from "../_components/UserProfile";
 import { Checkbox } from "@/components/ui/checkbox";
 import RecentSupport from "../_components/supporters/RecentSupportersHome";
 import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import Cookies from "js-cookie";
 import { HomeSkeleton } from "../_components/skeletons/HomeSkeleton";
 import { usePathname } from "next/navigation";
 
 export default function Home() {
-	const [transactions, setTransactions] = useState<Transaction[]>([]);
-	const [userId, setUserId] = useState<string | null>(null);
-	useEffect(() => {
-		const storedUserId: string | null = localStorage.getItem("userId");
-		setUserId(storedUserId);
-	}, []);
-	const [selectedAmount, setSelectedAmount] = useState<string | null>(null);
-	const pathname = usePathname();
-	console.log("User ID:", userId);
-	const accessToken = Cookies.get("accessToken");
-	const fetchData = async () => {
-		if (!userId) {
-			console.warn("No userId found, skipping fetch.");
-			return;
-		}
-		console.log(accessToken, "home");
-		try {
-			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_API_URL}/donation/received/${userId}`,
-				{
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: "Bearer " + accessToken,
-					},
-				}
-			);
-			if (!res.ok) throw new Error("Failed to fetch user data");
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const storedUserId: string | null = localStorage.getItem("userId");
+    setUserId(storedUserId);
+  }, []);
+  const [selectedAmount, setSelectedAmount] = useState<string | null>(null);
+  const pathname = usePathname();
+  console.log("User ID:", userId);
+  const accessToken = Cookies.get("accessToken");
+  const fetchData = async () => {
+    setIsLoading(true);
+    if (!userId) {
+      console.warn("No userId found, skipping fetch.");
+      return;
+    }
+    console.log(accessToken, "home");
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/donation/received/${userId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+      if (!res.ok) throw new Error("Failed to fetch user data");
 
-			const resJson = await res.json();
-			console.log("API Response:", resJson);
+      const resJson = await res.json();
+      console.log("API Response:", resJson);
 
-			setTransactions(
-				Array.isArray(resJson.donations) ? resJson.donations : []
-			);
-		} catch (error) {
-			console.error("Error fetching transactions:", error);
-			setTransactions([]);
-		}
-	};
-	const filteredTransactions = selectedAmount
-		? transactions.filter(
-				(transaction) => `$${transaction.amount}` === selectedAmount
-		  )
-		: transactions;
+      setTransactions(
+        Array.isArray(resJson.donations) ? resJson.donations : []
+      );
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching transactions:", error);
+      setTransactions([]);
+    }
+  };
 
-	useEffect(() => {
-		fetchData();
-		console.log("wooorkinggg");
-	}, [userId, pathname]);
+  const filteredTransactions = selectedAmount
+    ? transactions.filter(
+        (transaction) => `$${transaction.amount}` === selectedAmount
+      )
+    : transactions;
 
-	console.log("Transactions:", transactions);
+  useEffect(() => {
+    fetchData();
+    console.log("wooorkinggg");
+  }, [userId, pathname]);
 
-	return (
-		<div className="w-[80%] ml-4 fixed right-0 top-0 h-screen flex flex-col bg-gray-primary text-black p-4 overflow-y-auto custom-scrollbar">
-			<UserProfile />
-			<div className="flex w-[80%] mx-auto p-4 justify-between">
-				<div className="w-full mt-6 font-bold">Recent transactions</div>
-				<div>
-					<Select onValueChange={setSelectedAmount}>
-						<SelectTrigger className=" flex border rounded-lg p-2">
-							<SelectValue className="text-lg" placeholder="Amount" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								<SelectItem value="$1">$1</SelectItem>
-								<SelectItem value="$2">$2</SelectItem>
-								<SelectItem value="$5">$5</SelectItem>
-								<SelectItem value="$10">$10</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-				</div>
-			</div>
+  console.log("Transactions:", transactions);
 
-			{transactions.length > 0 ? (
-				<div className="w-[80%] my-auto p-5 overflow-y-auto custom-scrollbar mx-auto mt-4">
-					{filteredTransactions.map((transaction) => (
-						<RecentSupport key={transaction.id} transaction={transaction} />
-					))}
-				</div>
-			) : (
-				<div className="w-full my-auto mx-auto p-5 text-center text-gray-500 mt-4">
-					{transactions && (
-						<>
-							<HomeSkeleton />
-							<HomeSkeleton />
-							<HomeSkeleton />
-							<HomeSkeleton />
-							<HomeSkeleton />
-							<HomeSkeleton />
-						</>
-					)}
-					{(!transactions || selectedAmount) &&
-						"Donation information is not available"}
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="w-[80%] ml-4 fixed right-0 top-0 h-screen flex flex-col bg-gray-primary text-black p-4 overflow-y-auto custom-scrollbar">
+      <UserProfile />
+      <div className="flex w-[80%] mx-auto p-4 justify-between">
+        <div className="w-full mt-6 font-bold">Recent transactions</div>
+        <div>
+          <Select onValueChange={setSelectedAmount}>
+            <SelectTrigger className=" flex border rounded-lg p-2">
+              <SelectValue className="text-lg" placeholder="Amount" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="$1">$1</SelectItem>
+                <SelectItem value="$2">$2</SelectItem>
+                <SelectItem value="$5">$5</SelectItem>
+                <SelectItem value="$10">$10</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      {isLoading && (
+        <div className="w-full my-auto p-5 overflow-y-auto custom-scrollbar mx-auto mt-4">
+          <HomeSkeleton />
+          <HomeSkeleton />
+          <HomeSkeleton />
+          <HomeSkeleton />
+          <HomeSkeleton />
+          <HomeSkeleton />
+        </div>
+      )}
+      {transactions.length > 0 ? (
+        <div className="w-[80%] mx-auto my-auto p-5 overflow-y-auto custom-scrollbar mt-4">
+          {filteredTransactions.map((transaction: any) => (
+            <RecentSupport
+              key={transaction.id}
+              transaction={transaction}
+              userId={userId}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="w-[80%] my-auto mx-auto p-5 text-center text-gray-500 mt-4">
+          <div>Donation information is not available</div>
+        </div>
+      )}
+    </div>
+  );
 }
